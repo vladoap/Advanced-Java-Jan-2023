456040054b2d4de522b6630f63913eee7f8873d1
Vladimir Apostolov <114332177+vladoap@users.noreply.github.com>
2023-01-31 21:39:08 +0200

    add tasks - Genercis

---
+++ generics/genericScale/Scale.java
@@ -0,0 +1,27 @@
+package generics.genericScale;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Scale<T extends Comparable<T>> {
+
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+
+    }
+
+    public T getHeavier() {
+        int result = left.compareTo(right);
+
+        if (result > 0) {
+            return left;
+        } else if (result < 0) {
+            return right;
+        }
+        return null;
+    }
+}
